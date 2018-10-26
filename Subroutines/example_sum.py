073375c2111bdc0460908d9498ec990d13c8b0ad
Jason Stajich <jason.stajich@gmail.com>
2018-10-26 15:35:37 -0700

    example 1 of sum

---
+++ Subroutines/example_sum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# expect to use subroutine like this:
+# sum = sum_list_numbers([2,3,4,5])
+# list_o_nums = [10,20,30,40]
+# sum = sum_list_numbers(list_o_nums)
+# print("sum is ",sum)
+
+def sum_list_numbers(input_numbers):
+    # this is the subroutine code
+    running_total = 0
+    for num in input_numbers:
+        running_total += num
+#    print(running_total)
+    return running_total
+
+# here we are actually using the subroutine
+list_o_nums = [10,20,30,40]
+sum = sum_list_numbers(list_o_nums)
+print("sum of", list_o_nums,"is",sum)
