ca8e0ab99590a8795a6090cb03889604b238000d
Jason Stajich <jasonstajich.phd@gmail.com>
2020-11-20 15:01:17 -0800

    set log file name

--- Proteins/iprscan.sh
+++ Proteins/iprscan.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-#SBATCH -p short -N 1 -n 16 --mem 4gb --out iprscan.%a.log
+#SBATCH -p short -N 1 -n 16 --mem 4gb --out iprscan.%A.log
 
 module load iprscan
 iprscan.sh --goterms --pathways -f tsv --cpu 8 -i examples.fa -f tsv > examples.iprscan.out
