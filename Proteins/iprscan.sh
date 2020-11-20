ea7deaa851e712cd1bb2b3fb12b9401d2a29cf40
Jason Stajich <jasonstajich.phd@gmail.com>
2020-11-20 15:00:58 -0800

    add iprscan

---
+++ Proteins/iprscan.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#SBATCH -p short -N 1 -n 16 --mem 4gb --out iprscan.%a.log
+
+module load iprscan
+iprscan.sh --goterms --pathways -f tsv --cpu 8 -i examples.fa -f tsv > examples.iprscan.out
